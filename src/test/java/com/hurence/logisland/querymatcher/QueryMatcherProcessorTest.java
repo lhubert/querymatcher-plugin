c7b86fd79f5584b7d2ba1015ac15f95329f9f4d9
Laurence Hubert <laurence.hubert@hurence.com>
2016-06-27 21:58:54 +0200

    changed an non working loop

--- src/test/java/com/hurence/logisland/querymatcher/QueryMatcherProcessorTest.java
+++ src/test/java/com/hurence/logisland/querymatcher/QueryMatcherProcessorTest.java
@@ -2,15 +2,15 @@ package com.hurence.logisland.querymatcher;
 
 import com.hurence.logisland.event.Event;
 import com.hurence.logisland.event.serializer.EventKryoSerializer;
-import com.hurence.logisland.integration.testUtils.EmbeddedKafkaEnvironment;
-import com.hurence.logisland.integration.testUtils.DocumentPublisher;
-import com.hurence.logisland.integration.testUtils.RulesPublisher;
+import com.hurence.logisland.integration.testutils.EmbeddedKafkaEnvironment;
+import com.hurence.logisland.integration.testutils.DocumentPublisher;
+import com.hurence.logisland.integration.testutils.RulesPublisher;
+import com.hurence.logisland.rules.KafkaRulesConsumer;
 import kafka.admin.TopicCommand;
 import kafka.consumer.ConsumerConfig;
 import kafka.consumer.ConsumerIterator;
 import kafka.consumer.KafkaStream;
 import kafka.javaapi.consumer.ConsumerConnector;
-import kafka.server.KafkaServer;
 import kafka.utils.TestUtils;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -29,15 +29,12 @@ public class QueryMatcherProcessorTest {
 
     static EmbeddedKafkaEnvironment context ;
 
-    static String docset = "/documents/frenchpress";
-    static String ruleset = "/rules/frenchpress";
-    static String docstopic = "docs";
-    static String rulestopic = "rules";
-    static String matchestopic = "matches";
+    static String docspath = "./resources/documents/frenchpress";
+    static String rulespath = "./resources/rules";
 
-    static String[] arg1 = new String[] {"--topic", docstopic, "--partitions", "1", "--replication-factor", "1"};
-    static String[] arg2 = new String[] {"--topic", rulestopic, "--partitions", "1","--replication-factor", "1" };
-    static String[] arg3 = new String[] {"--topic", matchestopic, "--partitions", "1","--replication-factor", "1" };
+    static String[] arg1 = new String[] {"--topic", "docs", "--partitions", "1", "--replication-factor", "1"};
+    static String[] arg2 = new String[] {"--topic", "rules", "--partitions", "1","--replication-factor", "1" };
+    static String[] arg3 = new String[] {"--topic", "matches", "--partitions", "1","--replication-factor", "1" };
 
 
     @BeforeClass
@@ -52,18 +49,18 @@ public class QueryMatcherProcessorTest {
         TopicCommand.createTopic(context.getZkClient(), new TopicCommand.TopicCommandOptions(arg3));
 
         // wait till all topics are created on all servers
-        TestUtils.waitUntilMetadataIsPropagated(scala.collection.JavaConversions.asScalaBuffer(context.getServers()), docstopic, 0, 5000);
-        TestUtils.waitUntilMetadataIsPropagated(scala.collection.JavaConversions.asScalaBuffer(context.getServers()), rulestopic, 0, 5000);
-        TestUtils.waitUntilMetadataIsPropagated(scala.collection.JavaConversions.asScalaBuffer(context.getServers()), matchestopic, 0, 5000);
+        TestUtils.waitUntilMetadataIsPropagated(scala.collection.JavaConversions.asScalaBuffer(context.getServers()), "docs", 0, 5000);
+        TestUtils.waitUntilMetadataIsPropagated(scala.collection.JavaConversions.asScalaBuffer(context.getServers()), "rules", 0, 5000);
+        TestUtils.waitUntilMetadataIsPropagated(scala.collection.JavaConversions.asScalaBuffer(context.getServers()), "matches", 0, 5000);
 
         try {
             // send documents in path dir to topic
             DocumentPublisher publisher = new DocumentPublisher();
-            publisher.publish(context, LuwakQueryMatcher.class.getResource(docset).getPath(), docstopic);
+            publisher.publish(context, docspath, "docs");
 
             // send the rules to rule topic
             RulesPublisher rpublisher = new RulesPublisher();
-            rpublisher.publish(context, LuwakQueryMatcher.class.getResource(ruleset).getPath(), rulestopic);
+            rpublisher.publish(context, rulespath, "rules");
         }
 
         catch (Exception e) {
@@ -74,31 +71,25 @@ public class QueryMatcherProcessorTest {
     @Test
     public void testProcess() throws Exception {
 
-
         // setup simple consumer for docs
         Properties consumerProperties = TestUtils.createConsumerProperties(context.getZkServer().connectString(), "group0", "consumer0", -1);
         ConsumerConnector consumer = kafka.consumer.Consumer.createJavaConsumerConnector(new ConsumerConfig(consumerProperties));
 
-
-        // deleting zookeeper information to make sure the consumer starts from the beginning
-        // see https://stackoverflow.com/questions/14935755/how-to-get-data-from-old-offset-point-in-kafka
-        context.getZkClient().delete("/consumers/group0");
-
         // reading rules
-        RulesConsumer rconsumer = new RulesConsumer();
-        List<MatchingRule> rules = rconsumer.consume(context, rulestopic);
+        KafkaRulesConsumer rconsumer = new KafkaRulesConsumer();
+        List<MatchingRule> rules = rconsumer.consume(context, "rules", "rules", "consumer0");
 
         LuwakQueryMatcher matcher = new LuwakQueryMatcher(rules);
         QueryMatcherProcessor processor = new QueryMatcherProcessor(matcher);
 
         // starting consumer for docs...
         Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
-        topicCountMap.put(docstopic, 1);
+        topicCountMap.put("docs", 1);
         Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer.createMessageStreams(topicCountMap);
-        KafkaStream<byte[], byte[]> stream = consumerMap.get(docstopic).get(0);
+        KafkaStream<byte[], byte[]> stream = consumerMap.get("docs").get(0);
         ConsumerIterator<byte[], byte[]> iterator = stream.iterator();
 
-        if (iterator.hasNext()) {
+        while (iterator.hasNext()) {
 
             final EventKryoSerializer deserializer = new EventKryoSerializer(true);
             ByteArrayInputStream bais = new ByteArrayInputStream(iterator.next().message());
@@ -109,10 +100,9 @@ public class QueryMatcherProcessorTest {
             for (Event e : result) {
                 System.out.println((String)e.get("name").getValue() + " : " + (String) e.get("matchingrules").getValue());
             }
+
             bais.close();
 
-        } else {
-            fail();
         }
 
         // cleanup
@@ -123,8 +113,6 @@ public class QueryMatcherProcessorTest {
 
     @AfterClass
     public static void closeAll() throws IOException {
-        for (KafkaServer server : context.getServers()) server.shutdown();
-        context.getZkClient().close();
-        context.getZkServer().shutdown();
+        context.close();
     }
 }
